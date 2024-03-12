62c5a01f58f8971007bd6addffb89e9d5b8d51b2
Yoonkeee <yoonkeee@gmail.com>
2024-03-12 12:11:43 +0900

    env 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,8 @@
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
-  // auth: process.env.VITE_DWHALE_TOKEN,
   auth: import.meta.env.VITE_DWHALE_TOKEN,
 });
-console.log(import.meta.env.VITE_DWHALE_TOKEN);
 
 export const getRepos = () =>
   octokit.rest.repos
