1a1dbd857218114ac47a04e897c55fc2ac46c5a8
Yoonkeee <yoonkeee@gmail.com>
2024-03-12 14:02:18 +0900

    env 설정

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { Router } from "./Router";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ChakraProvider resetCSS>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={Router} />
+        <Analytics />
       </QueryClientProvider>
     </ChakraProvider>
   </RecoilRoot>,
